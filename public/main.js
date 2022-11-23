c847ff9ef9a5f44d03bcb486ebda66edd0d100b3
Ridge <ridgechristensen28@mail.com>
2022-11-23 15:01:25 -0700

    With 2nd button request

--- public/main.js
+++ public/main.js
@@ -3,3 +3,15 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+const btn2 = document.getElementById('button2')
+
+const getCat = () => {
+    axios.get('http://ec2-34-209-12-41.us-west-2.compute.amazonaws.com/api/cat')
+    .then(res => {
+        alert(res.data)
+        .catch(err => console.log(err))
+    })
+}
+
+btn2.addEventListener('click', getCat)
